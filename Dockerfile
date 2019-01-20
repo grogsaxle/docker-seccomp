73b96d00b975a1f02b9d76a3aca0418c26ff6893
grogsaxle <ryan.goltry@gmail.com>
2019-01-19 22:31:48 -0500

    Updated Docs and more tests

--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,7 @@ from python:3.7-alpine3.7
 
 COPY src/run.py /run.py
 
+RUN find /
+
 CMD python run.py
 
--- src/run.py
+++ src/run.py
@@ -1,28 +1,60 @@
-
+import ctypes
 import os
+from pathlib import Path
 import platform
+import resource
+import subprocess
+import sys
+import time
+
+syscall = ctypes.CDLL(None).syscall
 
 def available(op):
-  print('''\u001b[32m''' + op[1] + ' available\u001b[0m')
+  print('''\u001b[32m''' + op[0] + ' available\u001b[0m')
 
 def unavailable(op):
-  print('''\u001b[31m''' + op[1] + ' unavilable\u001b[0m')
+  print('''\u001b[31m''' + op[0] + ' unavilable\u001b[0m')
 
 def try_chdir():
   os.chdir('/tmp')
+  subprocess.Popen(['/usr/bin/cd', '/tmp'], stdout=subprocess.PIPE) 
+
+def try_chmod():
+  filename = '/tmp/test.txt'
+  Path(filename).touch()
+  os.chmod(filename, 444)
+
+def try_getpid():
+  syscall(39)
+
+def try_setrlimit():
+  resource.setrlimit(resource.RLIMIT_CPU, (2, 2)) 
+
+
+def try_waitpid():
+  pid = os.fork()
+  if not pid:
+    time.sleep(2)
+    sys.exit(0)
+  else:
+    os.waitpid(pid, 0)
+
 
 
 ops = [
-  [platform.uname, 'uname'],
-  [try_chdir, 'chdir' ]
+  ['uname', platform.uname],
+  ['chdir', try_chdir],
+  ['chmod', try_chmod],
+  ['getpid', try_getpid],
+  ['setrlimit', try_setrlimit],
+  ['waitpid', try_waitpid]
 ]
 
 for op in ops:
   try:
-    op[0]()
+    op[1]()
     available(op)
-  except:
+  except Exception as e:
+    print(e)
     unavailable(op)
   
-
-
